f08287786839d2bb5bfb94bc466317111c63bf74
Guna B <bgunaINR005055@BLR.ALLEGISINDIA.COM>
2021-01-24 00:51:44 +0530

    Final Code

---
+++ src/ProjectImages/ProjectImages.js
@@ -0,0 +1,46 @@
+import Pic from "../images/Pic.jpeg";
+import nopic from "../images/nopic.jpg";
+import choosefile from "../images/ic_input_file.png";
+import input_file from "../images/ic_photo.png";
+import sticker from "../images/ic_sticker.png";
+import send from "../images/ic_send.png";
+import wave_hand from "../images/14-wave_hand.png";
+import lego1 from "../images/lego1.png";
+import lego2 from "../images/lego2.png";
+import lego3 from "../images/lego3.png";
+import lego4 from "../images/lego4.png";
+import lego5 from "../images/lego5.png";
+import lego6 from "../images/lego6.png";
+import mimi1 from "../images/mimi1.gif";
+import mimi2 from "../images/mimi2.gif";
+import mimi4 from "../images/mimi4.gif";
+import mimi5 from "../images/mimi5.gif";
+import mimi6 from "../images/mimi6.gif";
+import mimi7 from "../images/mimi7.gif";
+import mimi8 from "../images/mimi8.gif";
+import mimi9 from "../images/mimi9.gif";
+
+const Images = {
+  Pic,
+  nopic,
+  choosefile,
+  input_file,
+  sticker,
+  send,
+  wave_hand,
+  lego1,
+  lego2,
+  lego3,
+  lego4,
+  lego5,
+  lego6,
+  mimi1,
+  mimi2,
+  mimi4,
+  mimi5,
+  mimi6,
+  mimi7,
+  mimi8,
+  mimi9,
+};
+export default Images;
---
+++ src/Services/firebase.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAPTIx_FoqWfth0E1r2HF0knhbt-n-1uFY",
+  authDomain: "webchat-947ce.firebaseapp.com",
+  projectId: "webchat-947ce",
+  storageBucket: "webchat-947ce.appspot.com",
+  messagingSenderId: "677946764953",
+  appId: "1:677946764953:web:fdd7f36c0800c4f0f99208",
+  measurementId: "G-281WX04R5M"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+  export default firebase;
+
+
